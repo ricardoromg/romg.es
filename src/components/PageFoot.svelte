ef3c9b6afc525492a680c4042802a7f695e055d5
Ricardo <220261272+ricardoromg@users.noreply.github.com>
2026-05-03 22:05:20 +0200

    Add favicon and put it in page foot

--- src/components/PageFoot.svelte
+++ src/components/PageFoot.svelte
@@ -1,3 +1,13 @@
 <section class="bottom">
-    <p>&copy; 2026 - <a href="https://romg.es">romg.es</a></p>
+    <p>
+        &copy; 2026 - <a href="https://romg.es">romg.es</a>
+        <img id="KFD" src="/favicon.svg" alt="a tiny rubber duck" />
+    </p>
 </section>
+
+<style>
+    #KFD {
+        height: 1rem;
+        margin: -0.2rem 0;
+    }
+</style>
