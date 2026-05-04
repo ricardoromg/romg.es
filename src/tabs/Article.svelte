1f87a863deea302aa77cb94a65899d98a2306e33
Ricardo <220261272+ricardoromg@users.noreply.github.com>
2026-05-04 09:14:09 +0200

    Fix 404 flashes when loading page #2

--- src/tabs/Article.svelte
+++ src/tabs/Article.svelte
@@ -7,19 +7,26 @@
     const articles = import.meta.glob("../articles/*.svelte");
 
     let ArticleContent: any;
+    let loaded: boolean = false;
 
     $: if (slug) {
         const loader = articles[`../articles/${slug}.svelte`];
         if (loader) {
-            loader().then((module: any) => {
-                ArticleContent = module.default;
-            });
+            loader()
+                .then((module: any) => {
+                    ArticleContent = module.default;
+                })
+                .finally(() => (loaded = true));
+        } else {
+            loaded = true;
         }
     }
 </script>
 
 {#if ArticleContent}
     <svelte:component this={ArticleContent} />
-{:else}
+{:else if loaded}
     <FourOFour />
+{:else}
+    <p class="center emph">Loading...</p>
 {/if}
