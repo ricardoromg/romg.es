1fec5cca625bc216f4422b9d8e7e3655c316e7c0
Ricardo <220261272+ricardoromg@users.noreply.github.com>
2026-05-04 19:25:33 +0200

    Implement Image component that gathers alt texts from alt.json in /public/ to ensure that all images have an alt text assigned

---
+++ src/components/Image.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let src: string = "/";
+    export let alt: string = "";
+    export let styleid: string = "";
+    export let styleclass: string = "";
+    import data from "../../public/alt.json";
+
+    $: if (!alt) {
+        const imageInAltJson = data.find((image) => image.src === src);
+        if (imageInAltJson) {
+            alt = imageInAltJson.alt;
+        }
+    }
+</script>
+
+<img id={styleid} class={styleclass} {src} {alt} />
--- src/components/PageFoot.svelte
+++ src/components/PageFoot.svelte
@@ -1,7 +1,15 @@
+<script>
+    import Image from "./Image.svelte";
+</script>
+
 <section class="bottom">
     <p>
         &copy; 2026 - <a href="https://romg.es">romg.es</a>
-        <img id="KFD" src="/favicon.svg" alt="a tiny rubber duck" />
+        <Image
+            styleid="KFDfooter"
+            src="/favicon.svg"
+            alt="A tiny rubber duck"
+        />
     </p>
 </section>
 
