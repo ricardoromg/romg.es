eefa42f0c8e56ab0315fbd771017ef2eb0e5052a
Ricardo <220261272+ricardoromg@users.noreply.github.com>
2026-05-05 07:57:43 +0200

    Remove explicit style tags from Image component and forward props

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
+    import data from "../../public/alt.json";
+
     export let src: string = "/";
     export let alt: string = "";
-    export let styleid: string = "";
-    export let styleclass: string = "";
-    import data from "../../public/alt.json";
 
     $: if (!alt) {
         const imageInAltJson = data.find((image) => image.src === src);
@@ -13,4 +12,4 @@
     }
 </script>
 
-<img id={styleid} class={styleclass} {src} {alt} />
+<img {...$$restProps} {src} {alt} />
--- src/components/PageFoot.svelte
+++ src/components/PageFoot.svelte
@@ -5,11 +5,6 @@
 <section class="bottom">
     <p>
         &copy; 2026 - <a href="https://romg.es">romg.es</a>
-        <Image
-            styleid="KFDfooter"
-            src="/favicon.svg"
-            alt="A tiny rubber duck"
-        />
+        <Image id="KFDfooter" src="/favicon.svg" alt="A tiny rubber duck" />
     </p>
 </section>
-
