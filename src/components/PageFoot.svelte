6fb1b1de300d7c51861fb55df3afb626acd5d0ad
Ricardo <220261272+ricardoromg@users.noreply.github.com>
2026-05-10 14:42:30 +0200

    Update PageFoot.svelte

--- src/components/PageFoot.svelte
+++ src/components/PageFoot.svelte
@@ -5,6 +5,12 @@
 <section class="bottom">
     <p>
         &copy; 2026 - <a href="https://romg.es">romg.es</a>
-        <img id="KFDfooter" src="/favicon.svg" alt="A tiny rubber duck" />
+        <img
+            id="KFDfooter"
+            src="/favicon.svg"
+            alt="A tiny rubber duck"
+            width="16px"
+            height="16px"
+        />
     </p>
 </section>
